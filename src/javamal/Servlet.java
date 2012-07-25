90a9e2196f79081aec3546ec9ea8369ff2bac943
jaeyeun <jaeyeun97@gmail.com>
2012-07-25 14:09:30 +0900

    Removed extra footnotes.

--- src/javamal/Servlet.java
+++ src/javamal/Servlet.java
@@ -8,32 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.imageio.*;
 
-/**
- * Servlet implementation class Servlet
- */
 @WebServlet("/servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    /**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+	public Servlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String init = request.getParameter("init");
 		String grammar = request.getParameter("grammar");
 		int depth = Integer.parseInt(request.getParameter("depth"));
 		double angle = Double.parseDouble(request.getParameter("angle"));
-		Tree myTree = new Tree(init,grammar,depth,angle);
+		Tree myTree = new Tree(init, grammar, depth, angle);
 		response.setContentType("image/gif");
-		ImageIO.write(myTree.getBufferedImage(),"GIF",response.getOutputStream());
-		//response.getOutputStream().println(myTree.finalPath);
+		ImageIO.write(myTree.getBufferedImage(), "GIF",
+				response.getOutputStream());
 	}
 }
